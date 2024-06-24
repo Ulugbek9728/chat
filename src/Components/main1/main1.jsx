2858918bf3c9d94733d6eab46ecbadca93ccb7b0
Ulugbek <ulugbekshukurov9728@gmail.com>
2024-06-24 17:57:51 +0500

    24.06.2024

--- src/Components/main1/main1.jsx
+++ src/Components/main1/main1.jsx
@@ -1,12 +1,78 @@
 import "./main1.scss"
 import {style} from "../../utils/style.js";
-import { Flex, Radio } from 'antd';
+import {Flex, Radio} from 'antd';
+
 import DarkMode from "../darkMode/darkMode.jsx";
+import {useEffect, useState} from "react";
 
 function Main1() {
+    const [filtrUser, setFiltrUser] = useState({
+        chatTema: 'Oбшение',
+        userGender: '',
+        srcGender: '',
+        userAge: '',
+        srcAge: {}
+    })
+    const [srcUserAge, setSrcUserAge] = useState([
+        {
+            age: "до 17 лет",
+            type: "",
+            chatTema: ['Oбшение',]
+        },
+        {
+            age: "от 18 до 21 года",
+            type: "",
+            chatTema: ['Oбшение', 'Флирт 18+',]
+        },
+        {
+            age: "от 22 до 25 года",
+            type: "",
+            chatTema: ['Oбшение', 'Флирт 18+',]
+        },
+        {
+            age: "от 26 до 35 года",
+            type: "",
+            chatTema: ['Oбшение', 'Флирт 18+',]
+        },
+        {
+            age: "старше 36 лет",
+            type: "",
+            chatTema: ['Oбшение', 'Флирт 18+',]
+        }
+    ])
+    const [disablet, setDisablet] = useState(false)
+
+    function srcAge(e, age) {
+        setSrcUserAge(prevValue =>
+            prevValue.map(user =>
+                user.age === age ?
+                    user.type === e ? {...user, type: ''} : {...user, type: 'ACTIVE'} : user
+            ))
+    }
+
+    function chatNow() {
+
+        const userAgeSrc = srcUserAge.filter((item) => item.type === "ACTIVE")
+
+        const userAllFilter = {
+            ...filtrUser,
+            srcAge: filtrUser.userGender === 'Некто' ? {} : userAgeSrc
+        }
+        console.log(userAllFilter)
+    }
+
+    useEffect(() => {
+        if (filtrUser.userGender === "Некто") {
+            setDisablet(true);
+            setFiltrUser({
+                ...filtrUser,
+                srcAge: {},
+                srcGender: 'Не важно'
+            })
+        } else setDisablet(false);
+    }, [filtrUser.userGender]);
 
     return (
-
         <div className='main'>
             <div className={`${style.container}`}>
                 <div className="grid grid-cols-2 gap-5 xl:gap-16 ">
@@ -20,62 +86,139 @@ function Main1() {
                             </div>
                             <p className='mt-5'>Тема общения:</p>
                             <Flex vertical gap="middle">
-                                <Radio.Group defaultValue="a" buttonStyle="solid" className='h-11 flex justify-between'>
-                                    <Radio.Button className='bg-slate-800 rounded-lg text-white h-full pt-1.5 px-16' value="a">Oбшение</Radio.Button>
-                                    <Radio.Button className='bg-slate-800 rounded-lg text-white h-full pt-1.5 px-16 ' value="b">Флирт 18+</Radio.Button>
-                                    <Radio.Button className='bg-slate-800 rounded-lg text-white h-full pt-1.5 px-16 ' value="c">Ролка</Radio.Button>
+                                <Radio.Group defaultValue='Oбшение' buttonStyle="solid"
+                                             className='h-11 flex justify-between'
+                                             onChange={(e) => {
+                                                 setFiltrUser({
+                                                     ...filtrUser,
+                                                     chatTema: e.target.value,
+                                                     userGender: '',
+                                                     srcGender: '',
+                                                     userAge: '',
+                                                     srcAge: {}
+                                                 })
+
+                                             }}>
+                                    <Radio.Button className='bg-slate-800 rounded-lg text-white h-full pt-1.5 px-16'
+                                                  value="Oбшение">Oбшение</Radio.Button>
+                                    <Radio.Button className='bg-slate-800 rounded-lg text-white h-full pt-1.5 px-16 '
+                                                  value="Флирт 18+">Флирт 18+</Radio.Button>
+                                    <Radio.Button className='bg-slate-800 rounded-lg text-white h-full pt-1.5 px-16 '
+                                                  value="Ролка">Ролка</Radio.Button>
                                 </Radio.Group>
                             </Flex>
                             <div className="grid grid-cols-2 gap-4 mt-3">
                                 <div className="w-full">
                                     <p>Ваш пол:</p>
                                     <Flex vertical gap="middle">
-                                        <Radio.Group defaultValue="a" buttonStyle="solid" className='h-11'>
-                                            <Radio.Button className='bg-slate-800 text-white h-full w-24 pt-1.5' value="a">Некто</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 text-white h-full w-20 pt-1.5' value="b">M</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 text-white h-full w-20 pt-1.5' value="c">Ж</Radio.Button>
+                                        <Radio.Group buttonStyle="solid" className='h-11'
+                                                     value={filtrUser.userGender}
+                                                     onChange={(e) => {
+                                                         setFiltrUser({
+                                                             ...filtrUser,
+                                                             userGender: e.target.value,
+                                                             userAge: e.target.value === "Некто" ? '' : filtrUser.userAge
+                                                         })
+                                                     }}>
+                                            {
+                                                filtrUser.chatTema !== "Флирт 18+" ?
+                                                    <Radio.Button className='bg-slate-800 text-white h-full w-24 pt-1.5'
+                                                                  value="Некто">Некто</Radio.Button>
+                                                    : ''
+                                            }
+
+                                            <Radio.Button className='bg-slate-800 text-white h-full w-20 pt-1.5'
+                                                          value="M">M</Radio.Button>
+                                            <Radio.Button className='bg-slate-800 text-white h-full w-20 pt-1.5'
+                                                          value="Ж">Ж</Radio.Button>
                                         </Radio.Group>
                                     </Flex>
                                 </div>
                                 <div className="w-full">
                                     <p>Пол собеседника:</p>
                                     <Flex vertical gap="middle">
-                                        <Radio.Group defaultValue="a" buttonStyle="solid" className='h-11'>
-                                            <Radio.Button className='bg-slate-800 text-white h-full w-24 pt-1.5' value="a">Не важно</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 text-white h-full w-20 pt-1.5' value="b">M</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 text-white h-full w-20 pt-1.5' value="c">Ж</Radio.Button>
+                                        <Radio.Group buttonStyle="solid" className='h-11'
+                                                     value={filtrUser.srcGender}
+                                                     onChange={(e) => {
+                                                         setFiltrUser({...filtrUser, srcGender: e.target.value})
+                                                     }}>
+                                            {
+                                                filtrUser.chatTema !== "Флирт 18+" ?
+                                                    <Radio.Button className='bg-slate-800 text-white h-full w-24 pt-1.5'
+                                                                  value="Не важно">Не важно</Radio.Button>
+                                                    : ''
+                                            }
+
+                                            <Radio.Button disabled={disablet}
+                                                          className={`bg-slate-800 text-white h-full w-20 pt-1.5`}
+                                                          value="M">M</Radio.Button>
+                                            <Radio.Button disabled={disablet}
+                                                          className='bg-slate-800 text-white h-full w-20 pt-1.5'
+                                                          value="Ж">Ж</Radio.Button>
                                         </Radio.Group>
                                     </Flex>
                                 </div>
                             </div>
                             <div className="grid grid-cols-2 gap-4 mt-3">
-                                <div className="flex flex-col gap-2">
-                                    <p>Пол собеседника:</p>
-                                    <Flex vertical={false} gap="middle">
-                                        <Radio.Group defaultValue="a" buttonStyle="solid" className=''>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="a">Oбшение</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="b">Флирт 18+</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="d">Ролка</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="e">Флирт 18+</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="f">Ролка</Radio.Button>
-                                        </Radio.Group>
-                                    </Flex>
-                                </div>
-                                <div className="flex flex-col gap-2">
-                                    <p>Пол собеседника:</p>
-                                    <Flex vertical={false} gap="middle">
-                                        <Radio.Group defaultValue="a" buttonStyle="solid" className=''>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="a">Oбшение</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="b">Флирт 18+</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="d">Ролка</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="e">Флирт 18+</Radio.Button>
-                                            <Radio.Button className='bg-slate-800 rounded-lg mt-2 w-full text-white h-11 pt-1.5' value="f">Ролка</Radio.Button>
-                                        </Radio.Group>
-                                    </Flex>
-                                </div>
+                                {
+                                    disablet ? '' : <div className="flex flex-col gap-2">
+                                        <p>Ваш возраст:</p>
+
+                                        <Flex gap="middle">
+                                            <Radio.Group
+                                                buttonStyle="solid"
+                                                onChange={(e) => {
+                                                    setFiltrUser({...filtrUser, userAge: e.target.value})
+                                                }}
+                                                style={{cursor: disablet ? 'not-allowed' : ''}}
+                                                disabled={disablet}
+                                                value={filtrUser.userAge}
+                                            >
+                                                {
+                                                    srcUserAge.filter(item => item?.chatTema?.includes(filtrUser.chatTema)).map((item, index) => {
+                                                            return (
+                                                                <Radio.Button key={index}
+                                                                              className={`bg-slate-800 mt-2 rounded-lg w-full text-white h-11 pt-1.5`}
+                                                                              value={item.age}
+                                                                >
+                                                                    {item?.age}
+                                                                </Radio.Button>
+                                                            )
+                                                        }
+                                                    )
+                                                }
+                                            </Radio.Group>
+                                        </Flex>
+
+                                    </div>
+                                }
+                                {
+                                    disablet ? '' : <div className="flex flex-col gap-2">
+                                        <p className='mb-1.5'>Возраст собеседника:</p>
+                                        {
+                                            srcUserAge.filter(item => item?.chatTema?.includes(filtrUser.chatTema)).map((item, index) =>
+                                                // eslint-disable-next-line react/jsx-key
+                                                <button
+                                                    key={index}
+                                                    disabled={disablet}
+                                                    className={`border border-white bg-slate-800 rounded-lg mt-0.4 w-full text-white h-11  ${item.type}
+                                                    hover:text-blue-600 text-sm`}
+                                                    onClick={() => srcAge("ACTIVE", item.age)}
+                                                >
+                                                    {item?.age}
+                                                </button>
+                                            )
+                                        }
+                                    </div>
+                                }
+
                             </div>
                             <div className="flex justify-center">
-                                <button className='text-lg text-white Partner mt-10 mb-5 px-9 py-2.5 my-20 ' >Начать чат</button>
+                                <button className='text-lg text-white Partner mt-10 mb-5 px-9 py-2.5 my-20'
+                                        onClick={() => chatNow()}
+
+                                >Начать чат
+                                </button>
 
                             </div>
 
