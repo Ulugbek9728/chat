d61fa333ac4ea3bdf49a58382d6eb78c70f4ea81
Ulugbek <ulugbekshukurov9728@gmail.com>
2024-07-18 17:14:02 +0500

    18.07.2024 admin page

--- src/admin/AllChats.jsx
+++ src/admin/AllChats.jsx
@@ -1,38 +1,51 @@
-import {Space, Table, Modal} from 'antd';
+import {Space, Table, Modal,Alert, Flex, Spin, Input } from 'antd';
 import axios from "axios";
 import {domen} from "../domen.jsx"
 import {useEffect, useState} from "react";
 import "./admin.scss";
 
+const { Search } = Input;
+
+
 const {Column, ColumnGroup} = Table;
+
+const contentStyle = {
+    padding: 50,
+    background: 'rgba(0, 0, 0, 0.05)',
+    borderRadius: 4,
+};
+const content = <div style={contentStyle} />;
 
 function AllChats() {
     const [tableParams, setTableParams] = useState({
         pagination: {
             current: 1,
             pageSize: 10,
-            total: 100
+            total: 100,
         },
     });
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [fulInfoAdmin] = useState(JSON.parse(localStorage.getItem("AdminInfo")));
     const [allChats, setAllChats] = useState([])
     const [messages, setMessages] = useState([])
-
+    const [messagesLoading, setMessagesLoading] = useState(false)
+    const [src, setSrc] = useState(null)
 
     useEffect(() => {
+
         return () => {
-            GetALLChat(tableParams.pagination.current, tableParams.pagination.pageSize)
+            GetALLChat(tableParams.pagination.current, tableParams.pagination.pageSize, src)
         }
 
     }, []);
 
-    function GetALLChat(page, pageSize) {
+    function GetALLChat(page, size,query) {
         axios.get(`${domen}/chat-admin/list`, {
             headers: {"Authorization": "Bearer " + fulInfoAdmin?.token},
             params: {
-                size: pageSize,
-                page: page - 1
+                size,
+                 page:page-1,
+                query:query
             }
         }).then((response) => {
             setAllChats(response?.data?.content);
@@ -40,7 +53,7 @@ function AllChats() {
                 ...tableParams,
                 pagination: {
                     pageSize: response.data.size,
-                    total: response.data.totalElements
+                    total: response.data.totalElements,
                 }
             })
         }).catch((error) => {
@@ -50,27 +63,72 @@ function AllChats() {
 
     const showModal = (e) => {
         setIsModalOpen(true);
+        setMessagesLoading(true)
         axios.get(`${domen}/message/${e}`, {
             headers: {"Authorization": "Bearer " + fulInfoAdmin?.token},
 
         }).then((response) => {
             console.log(response.data)
+            setMessagesLoading(false)
+
             setMessages(response.data)
         }).catch((error) => {
+            setMessagesLoading(false)
             console.log(error)
         })
     };
-
     return (
         <div>
             <Modal title="Basic Modal"
                    open={isModalOpen}
                    onOk={()=>setIsModalOpen(false)}
                    onCancel={()=>setIsModalOpen(false)}>
-                <div className="h-96 overflow-y-auto">
+                {
+                    messagesLoading ? <Flex gap="middle" vertical className="h-96">
+                        <Spin tip="Loading...">
+                            <Alert
+                                message="Malumot yuklanmoqda"
+                                description="Iltimos oynani yopmang!"
+                                type="info"
+                            />
+                        </Spin>
+                    </Flex>:
+                        <div className="h-96 overflow-y-auto">
+                            {messages?.map(item => {
+                                const oneMessage = messages[0].senderId;
+                                return (
+                                    <div key={item?.messageId}>
+                                        {
+                                            oneMessage=== item?.senderId ?
+                                                <div className="flex justify-end mt-1"
+                                                     key={item?.messageId}>
+                                                    <div
+                                                        className="bg-blue-200 text-black p-2 rounded-tl-2xl rounded-bl-2xl rounded-tr-2xl max-w-xs">
+                                                        {item?.content}
+                                                    </div>
+                                                </div>
+                                                :
+                                                <div className="flex mt-1" key={item?.messageId}>
+                                                    <div
+                                                        className="bg-gray-300 text-black p-2 rounded-tl-2xl rounded-br-2xl rounded-tr-2xl max-w-xs">
+                                                        {item?.content}
+                                                    </div>
+                                                </div>
+                                        }
+                                    </div>
+                                )
+                            })}
+                        </div>
 
-                </div>
+
+                }
+
             </Modal>
+            <Search placeholder="chat search text" className="w-96 mb-3" enterButton
+                    onSearch={(e)=> {
+                        setSrc(e)
+                        e==="" ? GetALLChat(1, tableParams.pagination.pageSize,) :
+                            GetALLChat(1, tableParams.pagination.pageSize, e)}}/>
             <Table
                 dataSource={allChats?.map(item => {
                     return {
@@ -87,7 +145,7 @@ function AllChats() {
                     total: tableParams.pagination.total,
                     pageSize: tableParams.pagination.pageSize,
                     onChange: (page, pageSize) => {
-                        GetALLChat(page, pageSize);
+                        GetALLChat(page, pageSize, src);
                     }
                 }}
             >
@@ -106,6 +164,7 @@ function AllChats() {
                             <Space size="middle">
                                 <a onClick={() => {
                                     showModal(record?.chatId)
+                                    setMessages([])
                                 }}>
                                     {record.LastMesage}
                                 </a>
